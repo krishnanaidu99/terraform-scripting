74be238fbd7d34369e9dd40e96452bf4e99cb192
krishnanaidu99 <129326410+krishnanaidu99@users.noreply.github.com>
2023-08-23 10:28:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "terraform-2" {
   ami                         = "ami-04b1c88a6bbd48f8e"
   instance_type               = "t2.micro"
-  key_name                    = "spidey-1"
+  key_name                    = "vvk"
   vpc_security_group_ids      = [aws_security_group.demosg.id]
   subnet_id                   = aws_subnet.public-subnet-1.id
   associate_public_ip_address = true
@@ -16,7 +16,7 @@ tags = {
 resource "aws_instance" "terraform-3" {
   ami                         = "ami-04b1c88a6bbd48f8e"
   instance_type               = "t2.micro"
-  key_name                    = "spidey-1"
+  key_name                    = "vvk"
   vpc_security_group_ids      = [aws_security_group.demosg.id]
   subnet_id                   = aws_subnet.public-subnet-1.id
   associate_public_ip_address = true
